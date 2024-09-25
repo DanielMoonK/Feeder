af060ad66d1579be23682bf6378aecd8d3fe058c
DanielMoonK <danielk213@nycstudents.net>
2024-09-25 12:07:25 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,17 @@ public class Main
     public static void main(String[] args)
     {
         Feeder f = new Feeder(500);
-        //System.out.println(f.getCurrentFood());
-        //int random = (int) ((Math.random() * 40) + 10);
-        //System.out.println(random);
-        System.out.print(f.simulateManyDays(10, 4));
-        System.out.println(" run days.");
+        f.simulateOneDay(12);
+        Feeder g = new Feeder(1000);
+        g.simulateOneDay(22);
+        Feeder h = new Feeder(100);
+        h.simulateOneDay(5);
+        
+        Feeder x = new Feeder(2400);
+        System.out.println(x.simulateManyDays(10, 4);
+        Feeder y = new Feeder(250);
+        System.out.println(y.simulateManyDays(10, 5);
+        Feeder z = new Feeder(0);
+        System.out.println(z. simulateManyDays(5, 10);
     }
 }
